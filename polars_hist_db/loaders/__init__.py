f6a345af0979d38dd5ca9642c17586fad9a09144
jr200 <jayshan+git@gmail.com>
2025-05-25 20:11:39 +0100

    move dsv loader to file package

--- polars_hist_db/loaders/__init__.py
+++ polars_hist_db/loaders/__init__.py
@@ -1,6 +1,6 @@
-from .dsv_loader import load_typed_dsv
-from .file_search import find_files
-from .ziptools import convert_zipped_csvs_to_parquet
+from .file.dsv_loader import load_typed_dsv
+from .file.file_search import find_files
+from .file.ziptools import convert_zipped_csvs_to_parquet
 
 __all__ = [
     "load_typed_dsv",

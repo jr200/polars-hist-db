35e6d78ad340f0d65bb3567ab62fa1ab4ccd869e
jr200 <jayshan+git@gmail.com>
2025-08-23 23:06:41 +0100

    audit log tracker should report both table schema and table name

--- polars_hist_db/core/audit_log_tracker.py
+++ polars_hist_db/core/audit_log_tracker.py
@@ -1,16 +1,20 @@
-from typing import Dict, Any, Callable, Awaitable, Optional, List
 from datetime import datetime
+import logging
+from typing import Dict, Any, Callable, Awaitable, Optional, List
 import pytz
 from sqlalchemy.engine import Connection
 
 from .audit import AuditOps
 
+LOGGER = logging.getLogger(__name__)
+
+TableUpdateCallback = Callable[[str, str, datetime], Awaitable[None]]
 
 class AuditLogTracker:
     _borg: Dict[str, Any] = {}
 
     last_known_updates: Dict[str, datetime]
-    table_update_callback: Optional[Callable[[str, datetime], Awaitable[None]]]
+    table_update_callback: Optional[TableUpdateCallback]
 
     def __init__(self):
         self.__dict__ = self._borg
@@ -22,7 +26,7 @@ class AuditLogTracker:
     def update_last_known_update(self, table_key: str, timestamp: datetime):
         self.last_known_updates[table_key] = timestamp
 
-    def set_table_update_callback(self, cb: Callable[[str, datetime], Awaitable[None]]):
+    def set_table_update_callback(self, cb: TableUpdateCallback):
         self.table_update_callback = cb
 
     def clear_updates(self):
@@ -44,7 +48,6 @@ class AuditLogTracker:
         if self.table_update_callback is None:
             raise ValueError("Developer Error: table_update_callback is not set")
 
-        print(f"poll_audit_log_task: {epoch_ms}")
         audit_ops = [AuditOps(schema) for schema in schemas]
         asof_timestamp = datetime.fromtimestamp(epoch_ms / 1000, tz=pytz.utc)
 
@@ -66,13 +69,13 @@ class AuditLogTracker:
                     if last_known is None or new_timestamp > last_known:
                         # Table has been updated, invoke callback
                         try:
-                            await self.table_update_callback(table_name, new_timestamp)
-                            print(
-                                f"Callback invoked for table {table_name} at {new_timestamp}"
+                            await self.table_update_callback(aops.schema, table_name, new_timestamp)
+                            LOGGER.debug(
+                                f"Callback invoked for table {aops.schema}.{table_name} at {new_timestamp}"
                             )
                         except Exception as e:
-                            print(
-                                f"Error in table update callback for {table_name}: {e}"
+                            LOGGER.error(
+                                f"Error in table update callback for {aops.schema}.{table_name}: {e}"
                             )
 
                         # Update the last known timestamp
